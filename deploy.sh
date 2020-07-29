40925915eea921a30d1698ec0f8feb5445e7b02e
i47uen <i47uen@ya.ru>
2020-07-29 14:48:31 +0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
- git push -f https://github.com/i47uen/vue-todo-app master:gh-pages
+git push -f https://github.com/i47uen/vue-todo-app master:gh-pages
 
 cd -
